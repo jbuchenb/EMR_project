61046deffec3bd91d87d3c8c252979d008645675
jbuchenb <32521978+jbuchenb@users.noreply.github.com>
2018-04-08 21:27:59 -0400

    Initial commit

---
+++ Demographic file.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr  4 22:55:24 2018
+
+@author: dan
+Demographic Info file
+
+"""
+patient_file = []
+
+patient_file.append(input('First Name ')+ ',')
+patient_file.append(input('Last Name ')+ ',')
+patient_file.append(input("Address ")+',')
+patient_file.append(input('apt ')+ ',')
+patient_file.append(input('City ')+ ',')
+patient_file.append(input('State Initials ')+',')
+patient_file.append(input('Zip code ')+',' )
+patient_file.append(input('Primary phone number (xxx-xxx-xxxx) ')+ ',')
+patient_file.append(input('Secondary phone number (xxx-xxx-xxxx) ')+ ',')
+patient_file.append(input('Social Security Number (XXX-XX-XXXX')+ ',')
+patient_file.append(input('Email Address ')+ ',')
+
+
---
+++ test_demographic.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr  7 14:39:36 2018
+
+@author: dan
+"""
+
+print(patient_file)
+help(list)
+#%%
+
+patient_file.index(patient_file)
